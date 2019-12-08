7144babf463d62949e5e728e25bcb22889a900e6
Stephane Cottereau <stefan.cottereau@gmail.com>
2019-12-08 19:38:35 +0000

    Refactoring exported sync actions using createActions from redux-actions

--- src/actions/todos/sync.js
+++ src/actions/todos/sync.js
@@ -1,6 +1,8 @@
 // SYNC ACTION CREATOR FUNCTIONS
+// eslint-disable-next-line no-unused-vars
 import { ADD_TODO, UPDATE_CURRENT, DELETE_TODO, LOAD_TODOS, REPLACE_TODO, SHOW_LOADER, HIDE_LOADER } from "./types";
-import { createAction } from 'redux-actions'
+//import { createAction } from 'redux-actions'
+import { createActions } from 'redux-actions'
 
 // export const updateCurrent = (val) => ({ type: UPDATE_CURRENT, payload: val })
 // export const loadTodos = (todos) => ({ type: LOAD_TODOS, payload: todos })
@@ -27,13 +29,33 @@ const fixeCase = (str) => {
 }
 
 // reproduce the pattern where the action creator takes one argument to feed the payload action property
-export const updateCurrent = createAction(UPDATE_CURRENT, fixeCase)
-export const loadTodos = createAction(LOAD_TODOS)
-export const addTodo = createAction(ADD_TODO)
-export const replaceTodo = createAction(REPLACE_TODO)
-export const deleteTodo = createAction(DELETE_TODO)
+// export const updateCurrent = createAction(UPDATE_CURRENT, fixeCase)
+// export const loadTodos = createAction(LOAD_TODOS)
+// export const addTodo = createAction(ADD_TODO)
+// export const replaceTodo = createAction(REPLACE_TODO)
+// export const deleteTodo = createAction(DELETE_TODO)
 
 // For hard coded, payload value, we pass a payload function creator
-export const showLoader = createAction(SHOW_LOADER, () => true)
-export const hideLoader = createAction(HIDE_LOADER, () => false)
+// export const showLoader = createAction(SHOW_LOADER, () => true)
+// export const hideLoader = createAction(HIDE_LOADER, () => false)
 
+// By default, all action creator are accessible by a camelCase reference to them,
+// which is the naming convention adopted before
+export const {
+  updateCurrent,
+  loadTodos,
+  addTodo,
+  replaceTodo,
+  deleteTodo,
+  showLoader,
+  hideLoader
+} = createActions({
+  UPDATE_CURRENT: fixeCase,
+  SHOW_LOADER: () => true,
+  HIDE_LOADER: () => false
+},
+  LOAD_TODOS, // map other actions with no redefined payload creator
+  ADD_TODO,
+  REPLACE_TODO,
+  DELETE_TODO
+)
